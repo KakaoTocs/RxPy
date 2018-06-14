d5d0990602168c1546ad58f24f2bdac9f3539668
kimjinu <kimjinu5555@gmail.com>
2018-06-14 22:48:10 +0900

    HelloWorld!
    시작

---
+++ 01_HelloWorld.py
@@ -0,0 +1,20 @@
+from rx import Observable, Observer
+
+def push_hello_world(observer):
+    observer.on_next("hello")
+    observer.on_next("world")
+    observer.on_completed()
+
+class PrintObserver(Observer):
+    def on_next(self, value):
+        print("Received {0}".format(value))
+
+    def on_completed(self):
+        print("Done!")
+
+    def on_error(self, error):
+        print("Error Occurred: {0}".format(error))
+
+if __name__ == '__main__':
+    source = Observable.create(push_hello_world)
+    source.subscribe(PrintObserver())
